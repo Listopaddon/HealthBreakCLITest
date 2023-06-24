0f385920a07bc1e2d5909fb70aaa3e46e4c36abe
Listopaddon <67251998+Listopaddon@users.noreply.github.com>
2023-06-24 10:47:01 +0300

    Add project files.

---
+++ HealthBreakCLITest/HealthBreakCLI.cs
@@ -0,0 +1,31 @@
+﻿
+namespace HealthBreakCLITest
+{
+    public class HealthBreakCLI
+    {
+        static HttpClient httpClient = new HttpClient();
+
+        public void IsValidURl(string url, int quantity)
+        {
+            List<bool> results = new List<bool>();
+
+            for (int i = 0; i < quantity; i++)
+            {
+                results.Add(CheckURL(url).Result);
+            }
+
+            foreach (bool result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private async Task<bool> CheckURL(string url)
+        {
+            using HttpRequestMessage requeste = new HttpRequestMessage(HttpMethod.Get, url);
+            using HttpResponseMessage response = await httpClient.SendAsync(requeste);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ HealthBreakCLITest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HealthBreakCLITest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HealthBreakCLI cLI = new HealthBreakCLI();
+
+            Console.Write("Enter url string - ");
+            string url = Console.ReadLine();
+            Console.Write("Quantity requests - ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            cLI.IsValidURl(url, quantity);
+        }
+    }
+}
